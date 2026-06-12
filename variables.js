da5150973b35684650830860f0fba11e39ff9c95
Tanish Mudgal <tanishmudgal55@gmail.com>
2026-06-12 15:51:50 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "tanish@example.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountID = 1000 can't change because it's a constant
+
+accountEmail = "hc@hc.com"
+accountPassword = "67890"
+accountCity = "Mumbai"
+let accountState
+console.log(accountID);
+console.table([accountEmail, accountPassword, accountCity, accountState])
